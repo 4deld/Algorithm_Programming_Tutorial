01f266bcf426189337afcebad13d6a5cb5ef2f64
Jaehyun Kim <rlapo@naver.com>
2022-05-08 10:06:06 +0900

    Create 3-23968.c
    bubble sort

---
+++ 3-23968.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int n, k;
+int main() {
+	int A[100000];
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - 1; j++) {
+			if (A[j] > A[j + 1]) {
+				int tmp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = tmp;
+				k--;
+			}
+			if (k == 0) {
+				printf("%d %d", A[j], A[j + 1]);
+				return 0;
+			}
+		}
+	}
+	printf("-1");
+
+}
